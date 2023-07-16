dc97bde714058770511a1a1bbacfb071f7687cb7
Juan Avendano <juanpablo_1810dev@hotmail.com>
2023-07-16 11:48:05 -0500

    se crean el menu principal

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,7 @@
+
+=
+src/Main.java,9\2\924f089edb4af5ee45c8a9d1129cd28e2b368138
+K
+src/model/ISchedulable.java,6\6\66dd9eecce49c74eeffc1391cafc40529152b6da
+P
+ src/model/AppointmentDoctor.java,9\c\9cbf96bcdafa8c48b397dda758a7946c937a0dc4
--- src/ui/UIMenu.java
+++ src/ui/UIMenu.java
@@ -1,4 +1,50 @@
 package ui;
 
+import model.Doctor;
+import model.Patient;
+
+import java.util.Scanner;
+
 public class UIMenu {
+
+    public static final String[] MONTHS ={"Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"};
+    public static Doctor doctorLogged;
+    public static Patient patientLogged;
+
+    public static void showMenu(){
+        System.out.println("Welcome to My Appointments");
+        System.out.println("Selecciona la opcion deseada");
+
+        int response = 0;
+
+        do{
+            System.out.println("1. Doctor");
+            System.out.println("2. Patient");
+            System.out.println("0. Salir");
+
+            Scanner sc = new Scanner(System.in);
+            response = Integer.valueOf(sc.nextLine());
+
+            switch (response){
+                case 1:
+                System.out.println("Doctor");
+                response=0;
+                authUser(1);
+                break;
+                case 2:
+                    response=0;
+                    authUser(2);
+                    break;
+                case 0:
+                    System.out.println("Thank you for you visit");
+
+                default:
+                    System.out.println("Please select a correct answer");
+            }
+
+
+        }while (response != 0);
+    }
+
+
 }
